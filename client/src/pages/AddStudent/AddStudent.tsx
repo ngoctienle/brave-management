511519edafea37dca5642b9d537f5d4bdd9b699e
Le Ngoc Tien <94373805+ngoctienle@users.noreply.github.com>
2022-12-03 23:38:28 +0700

    (client): remove dark css & update paginate

--- client/src/pages/AddStudent/AddStudent.tsx
+++ client/src/pages/AddStudent/AddStudent.tsx
@@ -8,13 +8,13 @@ export default function AddStudent() {
             type='email'
             name='floating_email'
             id='floating_email'
-            className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500'
+            className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0'
             placeholder=' '
             required
           />
           <label
             htmlFor='floating_email'
-            className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500'
+            className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600'
           >
             Email address
           </label>
@@ -28,21 +28,9 @@ export default function AddStudent() {
                   id='gender-1'
                   type='radio'
                   name='gender'
-                  className='h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
+                  className='h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500'
                 />
-                <label htmlFor='gender-1' className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
-                  Male
-                </label>
-              </div>
-              <div className='mb-4 flex items-center'>
-                <input
-                  defaultChecked
-                  id='gender-2'
-                  type='radio'
-                  name='gender'
-                  className='h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
-                />
-                <label htmlFor='gender-2' className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+                <label htmlFor='gender-1' className='ml-2 text-sm font-medium text-gray-900'>
                   Female
                 </label>
               </div>
@@ -52,9 +40,9 @@ export default function AddStudent() {
                   id='gender-3'
                   type='radio'
                   name='gender'
-                  className='h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
+                  className='h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500'
                 />
-                <label htmlFor='gender-3' className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+                <label htmlFor='gender-3' className='ml-2 text-sm font-medium text-gray-900'>
                   Other
                 </label>
               </div>
@@ -66,13 +54,13 @@ export default function AddStudent() {
             type='text'
             name='country'
             id='country'
-            className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500'
+            className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0'
             placeholder=' '
             required
           />
           <label
             htmlFor='country'
-            className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500'
+            className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600'
           >
             Country
           </label>
@@ -84,13 +72,13 @@ export default function AddStudent() {
               pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
               name='first_name'
               id='first_name'
-              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500'
+              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0'
               placeholder=' '
               required
             />
             <label
               htmlFor='first_name'
-              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500'
+              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600'
             >
               First Name
             </label>
@@ -100,13 +88,13 @@ export default function AddStudent() {
               type='text'
               name='last_name'
               id='last_name'
-              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500'
+              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0'
               placeholder=' '
               required
             />
             <label
               htmlFor='last_name'
-              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500'
+              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600'
             >
               Last Name
             </label>
@@ -118,13 +106,13 @@ export default function AddStudent() {
               type='text'
               name='avatar'
               id='avatar'
-              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500'
+              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0'
               placeholder=' '
               required
             />
             <label
               htmlFor='avatar'
-              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500'
+              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600'
             >
               Avatar Base64
             </label>
@@ -134,13 +122,13 @@ export default function AddStudent() {
               type='text'
               name='btc_address'
               id='btc_address'
-              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500'
+              className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0'
               placeholder=' '
               required
             />
             <label
               htmlFor='btc_address'
-              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500'
+              className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600'
             >
               BTC Address
             </label>
@@ -149,7 +137,7 @@ export default function AddStudent() {
 
         <button
           type='submit'
-          className='w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto'
+          className='w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto'
         >
           Submit
         </button>
--- client/src/pages/Students/Students.tsx
+++ client/src/pages/Students/Students.tsx
@@ -1,11 +1,14 @@
 import { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
+import classnames from 'classnames'
 
 import LoadingUI from 'components/LoadingUI'
 
 import { getStudents } from 'apis/students.api'
 import { useQueryString } from 'utils/hooks'
+
+const LIMIT = 10
 
 export default function Students() {
   const queryString: { page?: string } = useQueryString()
@@ -13,92 +16,129 @@ export default function Students() {
 
   const { data, isLoading } = useQuery({
     queryKey: ['StudentList', page],
-    queryFn: () => getStudents(page, 10)
+    queryFn: () => getStudents(page, LIMIT),
+    keepPreviousData: true
   })
+
+  const totalStudentCount = Number(data?.headers['x-total-count'] || 0)
+  const totalPage = Math.ceil(totalStudentCount / LIMIT)
 
   return (
     <Fragment>
-      <h1 className='text-lg'>Students</h1>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-lg'>Students</h1>
+        <Link
+          to='/students/add'
+          className='rounded-lg border border-blue-500 bg-transparent px-5 py-2.5 text-sm font-medium text-blue-500 transition-colors hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300'
+        >
+          Add Student
+        </Link>
+      </div>
       {isLoading ? (
         <LoadingUI />
       ) : (
-        <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
-          <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
-            <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
-              <tr>
-                <th scope='col' className='py-3 px-6'>
-                  #
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  Avatar
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  Name
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  Email
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  <span className='sr-only'>Action</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.data.map((student) => (
-                <tr
-                  className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'
-                  key={student.id}
-                >
-                  <td className='py-4 px-6'>{student.id}</td>
-                  <td className='py-4 px-6'>
-                    <img src={student.avatar} alt='student' className='h-5 w-5' />
-                  </td>
-                  <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white'>
-                    {student.last_name}
+        <Fragment>
+          <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
+            <table className='w-full text-left text-sm text-gray-500'>
+              <thead className='bg-gray-50 text-xs uppercase text-gray-700'>
+                <tr>
+                  <th scope='col' className='py-3 px-6'>
+                    #
                   </th>
-                  <td className='py-4 px-6'>{student.email}</td>
-                  <td className='py-4 px-6 text-right'>
-                    <Link
-                      to='/students/1'
-                      className='mr-5 font-medium text-blue-600 hover:underline dark:text-blue-500'
-                    >
-                      Edit
-                    </Link>
-                    <button className='font-medium text-red-600 dark:text-red-500'>Delete</button>
-                  </td>
+                  <th scope='col' className='py-3 px-6'>
+                    Avatar
+                  </th>
+                  <th scope='col' className='py-3 px-6'>
+                    Name
+                  </th>
+                  <th scope='col' className='py-3 px-6'>
+                    Email
+                  </th>
+                  <th scope='col' className='py-3 px-6'>
+                    <span className='sr-only'>Action</span>
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {data?.data.map((student) => (
+                  <tr className='border-b bg-white hover:bg-gray-50' key={student.id}>
+                    <td className='py-4 px-6'>{student.id}</td>
+                    <td className='py-4 px-6'>
+                      <img src={student.avatar} alt='student' className='h-5 w-5' />
+                    </td>
+                    <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 '>
+                      {student.last_name}
+                    </th>
+                    <td className='py-4 px-6'>{student.email}</td>
+                    <td className='py-4 px-6 text-right'>
+                      <Link to='/students/1' className='mr-5 font-medium text-blue-600 hover:underline'>
+                        Edit
+                      </Link>
+                      <button className='font-medium text-red-600'>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className='mt-6 flex justify-center'>
+            <nav aria-label='Page navigation example'>
+              <ul className='inline-flex -space-x-px'>
+                <li>
+                  {page === 1 ? (
+                    <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700'>
+                      Previous
+                    </span>
+                  ) : (
+                    <Link
+                      to={`/students?page=${page - 1}`}
+                      className='rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 '
+                    >
+                      Previous
+                    </Link>
+                  )}
+                </li>
+                {Array(totalPage)
+                  .fill(0)
+                  .map((_, index) => {
+                    const pageNumber = index + 1
+                    const isActive = page === pageNumber
+                    return (
+                      <li key={pageNumber}>
+                        <Link
+                          className={classnames(
+                            'border border-gray-300 py-2 px-3 leading-tight text-gray-500  hover:bg-gray-100 hover:text-gray-700',
+                            {
+                              'bg-gray-100 text-gray-700': isActive,
+                              'bg-white text-gray-500': !isActive
+                            }
+                          )}
+                          to={`/students?page=${pageNumber}`}
+                        >
+                          {pageNumber}
+                        </Link>
+                      </li>
+                    )
+                  })}
+                <li>
+                  {page === totalPage ? (
+                    <span className='cursor-not-allowed rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700'>
+                      Next
+                    </span>
+                  ) : (
+                    <Link
+                      className='rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700'
+                      to={`students?page=${page + 1}`}
+                    >
+                      Next
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </Fragment>
       )}
-      <div className='mt-6 flex justify-center'>
-        <nav aria-label='Page navigation example'>
-          <ul className='inline-flex -space-x-px'>
-            <li>
-              <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
-                Previous
-              </span>
-            </li>
-            <li>
-              <a
-                className='border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500  hover:bg-gray-100 hover:text-gray-700'
-                href='/students?page=8'
-              >
-                1
-              </a>
-            </li>
-            <li>
-              <a
-                className='rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
-                href='/students?page=1'
-              >
-                Next
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
     </Fragment>
   )
 }
